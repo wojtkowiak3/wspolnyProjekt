768c371f7301fbd0510500bcdee46bfa64716bfd
Wojtkowiak3 <albert.wojtkowiak@uczen.zsk.poznan.pl>
2023-02-23 09:57:38 +0100

    code in program.cs

--- projekt2/Program.cs
+++ projekt2/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace projekt2
+﻿using projekt2.classes;
+
+namespace projekt2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string CsvFilePath = "C:\\xampp\\htdocs\\2023_02_23\\wspolnyProjekt\\projekt2\\population.csv";
+
+            CsvReader csvReader = new CsvReader(CsvFilePath);
+            City[] cities = csvReader.ReadFirstNCities(3);
+
+            foreach (City city in cities)
+            {
+                Console.WriteLine("Miasto");
+            }
         }
     }
 }
