e3b799e319eec8d82544f08ed5ae5738ae304a71
Wojtkowiak3 <albert.wojtkowiak@uczen.zsk.poznan.pl>
2023-02-23 09:21:36 +0100

    update

---
+++ projekt2/classes/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt2
+{
+    internal class City
+    {
+        public string CityName = "";
+
+        public City(string _CityName)
+        {
+            CityName = _CityName;
+        }
+    }
+}
