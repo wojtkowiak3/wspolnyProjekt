64f1308c935a8de99e9e6e0b39fd2767fc3ad85d
Wojtkowiak3 <albert.wojtkowiak@uczen.zsk.poznan.pl>
2023-02-23 09:30:23 +0100

    added CsvReader

---
+++ projekt2/classes/CsvReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt2.classes
+{
+    internal class CsvReader
+    {
+        private string CsvFilePath = "";
+
+        public CsvReader(string _csvFilePath)
+        {
+            CsvFilePath = _csvFilePath;
+        }
+
+        public City[] ReadFirstNCities(uint nCities)
+        {
+            return null;
+        }
+    }
+}
